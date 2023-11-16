055cf241da64d7fca622c4dbed45a85a71d5290f
Alireza Sohrabizadeh <asohrabi@c2r2p7.hive.fi>
2023-11-16 13:22:28 +0200

    Failed due to not handling errors correctly

---
+++ rush-00/ex00/rush03.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asohrabi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 11:00:55 by asohrabi          #+#    #+#             */
+/*   Updated: 2023/07/01 16:19:35 by asohrabi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	big_con(int z, int r, int i, int j)
+{
+	if ((z == 1 && r == 1) || (z == j && r == 1))
+		ft_putchar('A');
+	else if ((z == 1 && r == i) || (z == j && r == i))
+		ft_putchar('C');
+	else if ((z == 1 && r < i) || (z < j && r == 1))
+		ft_putchar('B');
+	else if ((z == j && r < i) || (z < j && r == i))
+		ft_putchar('B');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int	a;
+	int	b;
+
+	a = 1;
+	b = 1;
+	while (a <= y)
+	{
+		while (b <= x)
+		{
+			big_con(a, b, x, y);
+			b++;
+		}
+		b = 1;
+		ft_putchar('\n');
+		a++;
+	}
+}
---
+++ rush-00/ex00/rush04.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asohrabi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 11:00:55 by asohrabi          #+#    #+#             */
+/*   Updated: 2023/07/01 14:18:40 by phtran           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	con(int t, int s)
+{
+	if (t == 1)
+		ft_putchar('A');
+	else if (t == s)
+		ft_putchar('C');
+	else if (t < s)
+		ft_putchar('B');
+}
+
+void	big_con(int z, int r, int i, int j)
+{
+	if ((z == 1 && r == 1) || (z == j && r == i))
+		ft_putchar('A');
+	else if ((z == 1 && r == i) || (z == j && r == 1))
+		ft_putchar('C');
+	else if ((z == 1 && r < i) || (z < j && r == 1))
+		ft_putchar('B');
+	else if ((z == j && r < i) || (z < j && r == i))
+		ft_putchar('B');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int	a;
+	int	b;
+
+	a = 1;
+	b = 1;
+	while (a <= y)
+	{
+		while (b <= x)
+		{
+			if (x == 1)
+				con(a, y);
+			else if (y == 1)
+				con(b, x);
+			else
+				big_con(a, b, x, y);
+			b++;
+		}
+		b = 1;
+		ft_putchar('\n');
+		a++;
+	}
+}
